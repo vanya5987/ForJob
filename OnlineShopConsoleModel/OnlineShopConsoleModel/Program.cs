f98086c4bb4f826d4ae3c3608f3433539c7de4ab
Поздняков Иван <126605809+vanya5987@users.noreply.github.com>
2024-11-19 12:51:18 +0300

    Add files via upload

---
+++ OnlineShopConsoleModel/OnlineShopConsoleModel/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace OnlineShopModel
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var context = new ShopDbContext();
+            context.Database.EnsureCreated(); 
+
+            var authService = new AuthService(context);
+            var productRepository = new ProductRepository(context);
+
+            try
+            {
+                authService.Register("user1", "password1");
+                Console.WriteLine("Пользователь зарегистрирован!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                authService.Login("user1", "password1");
+                Console.WriteLine("Успешный вход!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            var topProducts = await productRepository.GetTopProductsAsync(5);
+            foreach (var product in topProducts)
+            {
+                Console.WriteLine($"Товар: {product.Name}");
+            }
+        }
+    }
+}
